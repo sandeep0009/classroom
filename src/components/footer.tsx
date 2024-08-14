e2703508c68b5c7c82086136be30139d1f3a7e01
sandeep0009 <500sandeepks@gmail.com>
2024-08-15 00:36:16 +0530

    completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+import { FaLinkedin, FaGithubSquare, FaFileAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -8,14 +10,21 @@ const Footer = () => {
           <div className="text-xl font-bold text-gray-800">
             ClassRoom
           </div>
-          <div className="flex flex-wrap lg:space-x-4">
-            <a href="#" className="text-gray-600 hover:text-gray-800">About</a>
-            <a href="#" className="text-gray-600 hover:text-gray-800">Contact</a>
-            <a href="#" className="text-gray-600 hover:text-gray-800">Privacy Policy</a>
+          <div className="flex flex-wrap lg:space-x-4 items-center">
+            <Link href="https://www.linkedin.com/in/sandeep-kumar-shah-915a34255/" target="_blank" rel="noopener noreferrer" className="flex items-center">
+              <FaLinkedin className="mr-1 rounded-lg" size={25} />
+            </Link>
+            <Link href="https://github.com/sandeep0009" target="_blank" rel="noopener noreferrer" className="flex items-center">
+              <FaGithubSquare className="mr-1 rounded-lg" size={25}  /> 
+            </Link>
+            <Link href="https://docs.google.com/document/d/1rUxmjbk0-8CRRT_oXDSOU_GmQ8fhv57hkspYCk9JWx8/edit?usp=drive_link" target="_blank" rel="noopener noreferrer" className="flex items-center">
+              <FaFileAlt className="mr-1 rounded-lg" size={25} /> 
+            </Link>
+            
           </div>
         </div>
-        <div className="mt-4  text-center text-sm text-gray-600">
-          © {new Date().getFullYear()} ClassRoom. All rights reserved.
+        <div className="mt-4 text-center text-sm text-gray-600">
+          © {new Date().getFullYear()} ClassRoom. All rights reserved.Create by Sandeep 
         </div>
       </div>
     </footer>
