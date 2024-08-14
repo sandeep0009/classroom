fe9e965cdea0c26e052e69e39929db243b912fd5
sandeep0009 <500sandeepks@gmail.com>
2024-08-14 18:51:06 +0530

    made some minor changes

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = process.env.BASE_URL;
 
 export const axiosInstance = (): AxiosInstance => {
   return axios.create({
