e2538cc62c90ffba3092e24cb6c0d211e612e6c1
sandeep0009 <500sandeepks@gmail.com>
2024-08-14 18:25:56 +0530

    made some minor changes

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -1,17 +1,12 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { useEffect, ComponentType, JSX } from 'react';
+import { useEffect, ComponentType } from 'react';
 
-interface Session {
-  user: {
-    role: 'principal' | 'teacher' | 'student';
-  };
-}
 
-export function withAuth<P extends JSX.IntrinsicAttributes>(WrappedComponent: ComponentType<P>) {
+export function withAuth<P extends Record<string, unknown>>(WrappedComponent: ComponentType<P>) {
   const AuthWrapper = (props: P) => {
     const { data: session, status } = useSession();
-    const router=useRouter()
+    const router = useRouter();
 
     useEffect(() => {
       if (status === 'loading') return;
